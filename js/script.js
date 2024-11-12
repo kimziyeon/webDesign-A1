ba9257b69f9a214edc01bc757eb4c641a9b5dc71
kimziyeon <aa40254037@gmail.com>
2024-11-12 17:35:42 +0900

    20241112 재연습

--- js/script.js
+++ js/script.js
@@ -1,59 +1,69 @@
+
 $(function () {
 
-    //슬라이드
-    function slide() {
-        $('.slide>ul').animate({ top: '-300px' }, 1000, function () {
-            $('.slide>ul').append($('.slide>ul>li').first())
-            $('.slide>ul').css({ top: '0' });
-        })
-    }
-    setInterval(slide, 2000)
-
-
-    //탭
-    $('.con1>ul>li').click(function () {
-        $('.con1>ul>li').removeClass('on');
-        $(this).addClass('on');
-
-        var i = $(this).index();
-
-        $('.tabcon').hide();
-        $('.tabcon').eq(i).show();
-        return false;
-    })
-
-
-    //네비메뉴
+    // 헤더
     $('nav>ul>li').mouseenter(function () {
         $('.sub').stop().slideDown();
     })
-
     $('nav>ul>li').mouseleave(function () {
         $('.sub').stop().slideUp();
     })
 
 
-    //키보드 포커싱
     $('nav>ul>li').focusin(function () {
-        $('.sub').slideDown();
-        $(this).addClass('on');
+        $('.sub').stop().slideDown();
+        $(this).children('a').addClass('on');
     })
-    $('.sub>li:last-child').focusout(function () {
-        $('nav>ul>li').removeClass('on');
+    $('nav>ul>li').focusout(function () {
+        $('.sub').stop().slideUp();
+        $(this).children('a').removeClass('on');
+
     })
-    $('nav li:last-child li:last-child').focusout(function () {
-        $('.sub').slideUp();
+
+    $('.sub>li').focusin(function () {
+        $(this).children('a').addClass('on');
+    })
+    $('.sub>li').focusout(function () {
+        $(this).children('a').removeClass('on');
+
+    })
+
+    // 슬라이드
+
+    function slide() {
+
+        $('.slide>ul').animate({ top: '-100%' }, 1000, function () {
+            $(this).append($('.slide>ul>li').first());
+            $('.slide>ul').css({ top: '0' });
+
+        })
+    }
+    setInterval(slide, 3000)
+
+    // tab
+
+    $('.cont>ul>li').click(function () {
+        $('.cont>ul>li').removeClass('on')
+        $(this).addClass('on')
+        $('.tab').hide();
+        $(this).children('.tab').show();
+    })
+
+    $('.cont>ul>li').focusin(function () {
+        $('.cont>ul>li').removeClass('on')
+        $(this).addClass('on')
+        $('.tab').hide();
+        $(this).children('.tab').show();
     })
 
 
-    //팝업
-    $('.pp').click(function () {
-        $('.popup').show();
-        return false;
+    // 팝업
+    $('.pop_on').click(function () {
+        $('.pop').show();
     })
 
-    $('.pbtn').click(function () {
-        $('.popup').hide();
+    $('.pop_off').click(function () {
+        $('.pop').hide();
     })
 
 })
